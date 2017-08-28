74d4b7cbef3f1b20b589833a0aff7d2c2fcffd7d
Charles Wang <charlesfoxw@gmail.com>
2017-08-28 03:31:56 -0700

    Working Version 1

--- HW1/rend.cpp
+++ HW1/rend.cpp
@@ -6,7 +6,11 @@
 /*   CS580 HW   */
 #include    "stdafx.h"  
 #include	"Gz.h"
+#include	<bitset>
 
+#define RGBA_DIMEMSION	4	/* RGBA -> 4D color */
+#define RGB_DIMEMSION	3	/* RGB -> 3D color */
+#define COLOR_LIMIT		4095
 
 GzRender::GzRender(int xRes, int yRes)
 {
@@ -15,17 +19,35 @@ GzRender::GzRender(int xRes, int yRes)
  -- allocate memory for framebuffer : 3 bytes(b, g, r) x width x height
  -- allocate memory for pixel buffer
  */
+	xres = (unsigned short)xRes;
+	yres = (unsigned short)yRes;
+
+	int resolution = 0;
+	resolution = xres * yres;
+	int frameBufferDepth = RGB_DIMEMSION * resolution;
+	framebuffer = new char[frameBufferDepth];
+	pixelbuffer = new GzPixel[resolution];
 }
 
 GzRender::~GzRender()
 {
 /* HW1.2 clean up, free buffer memory */
-
+	delete[] framebuffer;
+	delete[] pixelbuffer;
 }
 
 int GzRender::GzDefault()
 {
 /* HW1.3 set pixel buffer to some default values - start a new frame */
+	GzPixel defaultPixel = {880, 880, 880, 1, 0};
+
+	int resolution = xres * yres;
+	for (int i = 0; i < resolution; i++) {
+		pixelbuffer[i] = defaultPixel;
+		framebuffer[RGB_DIMEMSION * i] = 880;
+		framebuffer[RGB_DIMEMSION * i + 1] = 880;
+		framebuffer[RGB_DIMEMSION * i + 2] = 880;
+	}
 
 	return GZ_SUCCESS;
 }
@@ -34,6 +56,11 @@ int GzRender::GzDefault()
 int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, GzIntensity a, GzDepth z)
 {
 /* HW1.4 write pixel values into the buffer */
+	if (i >= 0 && i < xres && j >= 0 && j < yres) {
+		int index = ARRAY(i, j);
+		GzPixel currentPixel = { r, g, b, a, z };
+		pixelbuffer[index] = currentPixel;
+	}
 
 	return GZ_SUCCESS;
 }
@@ -42,7 +69,15 @@ int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, G
 int GzRender::GzGet(int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b, GzIntensity *a, GzDepth *z)
 {
 /* HW1.5 retrieve a pixel information from the pixel buffer */
-
+	if (i >= 0 && i < xres && j >= 0 && j < yres) {
+		int index = ARRAY(i, j);
+		GzPixel currentPixel = pixelbuffer[index];
+		*r = currentPixel.red;
+		*g = currentPixel.green;
+		*b = currentPixel.blue;
+		*a = currentPixel.alpha;
+		*z = currentPixel.z;
+	}
 	return GZ_SUCCESS;
 }
 
@@ -50,7 +85,41 @@ int GzRender::GzGet(int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b
 int GzRender::GzFlushDisplay2File(FILE* outfile)
 {
 /* HW1.6 write image to ppm file -- "P6 %d %d 255\r" */
+	fprintf(outfile, "P6 %d %d 255\n", xres, yres);
+	int resolution = xres * yres;
+	for (int i = 0; i < resolution; i++) {
+		GzPixel currentPixel = pixelbuffer[i];
 	
+		GzIntensity gotRed = currentPixel.red;
+		GzIntensity gotGreen = currentPixel.green;
+		GzIntensity gotBlue = currentPixel.blue;
+		if (currentPixel.red < 0)
+			gotRed = 0;
+		if (currentPixel.red > COLOR_LIMIT)
+			gotRed = COLOR_LIMIT;
+		if (currentPixel.green < 0)
+			gotGreen = 0;
+		if (currentPixel.green > COLOR_LIMIT)
+			gotGreen = COLOR_LIMIT;
+		if (currentPixel.blue < 0)
+			gotBlue = 0;
+		if (currentPixel.blue > COLOR_LIMIT)
+			gotBlue = COLOR_LIMIT;
+
+		GzIntensity red2 = gotRed >> 4;
+		char redValue = (char)(red2 & 0xFF);
+		GzIntensity green2 = gotGreen >> 4;
+		char greenValue = (char)(green2 & 0xFF);
+		GzIntensity blue2 = gotBlue >> 4;
+		char blueValue = (char)(blue2 & 0xFF);
+
+		char color[3];
+		color[0] = redValue;
+		color[1] = greenValue;
+		color[2] = blueValue;
+		fwrite(color, 1, 3, outfile);
+	}
+
 	return GZ_SUCCESS;
 }
 
@@ -61,6 +130,39 @@ int GzRender::GzFlushDisplay2FrameBuffer()
 	- CAUTION: when storing the pixels into the frame buffer, the order is blue, green, and red 
 	- NOT red, green, and blue !!!
 */
+	int resolution = xres * yres;
+	for (int i = 0; i < resolution; i++) {
+		GzPixel currentPixel = pixelbuffer[i];
+
+		GzIntensity gotRed = currentPixel.red;
+		GzIntensity gotGreen = currentPixel.green;
+		GzIntensity gotBlue = currentPixel.blue;
+		if (currentPixel.red < 0)
+			gotRed = 0;
+		if (currentPixel.red > COLOR_LIMIT)
+			gotRed = COLOR_LIMIT;
+		if (currentPixel.green < 0)
+			gotGreen = 0;
+		if (currentPixel.green > COLOR_LIMIT)
+			gotGreen = COLOR_LIMIT;
+		if (currentPixel.blue < 0)
+			gotBlue = 0;
+		if (currentPixel.blue > COLOR_LIMIT)
+			gotBlue = COLOR_LIMIT;
+
+		GzIntensity red2 = gotRed >> 4;
+		char redValue = (char) (red2 & 0xFF);
+		framebuffer[RGB_DIMEMSION * i + 2] = redValue;
+
+		GzIntensity green2 = gotGreen >> 4;
+		char greenValue = (char)(green2 & 0xFF);
+		framebuffer[RGB_DIMEMSION * i + 1] = greenValue;
+
+		GzIntensity blue2 = gotBlue >> 4;
+		char blueValue = (char)(blue2 & 0xFF);
+		framebuffer[RGB_DIMEMSION * i] = blueValue;
+	}
+
 
 	return GZ_SUCCESS;
 }
